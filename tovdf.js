9a32916904103d2a8cb21ad465f63c8376eabe0f
Xavier Sellier <xsellier@binogure-studio.com>
2019-10-16 16:11:36 -0400

    Add steam achievement converter Translate steam achievements in french

---
+++ tovdf.js
@@ -0,0 +1,45 @@
+const yaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
+
+const langMatch = {
+  de: 'german',
+  en: 'english',
+  es: 'spanish',
+  pt: 'portuguese',
+  fr: 'french',
+  ru: 'russian'
+}
+const language_directory = path.resolve('achievements')
+const encoding = {
+  encoding: 'utf-8'
+}
+
+console.log('"lang" {')
+
+const files = fs.readdirSync(language_directory)
+files.map((filename) => {
+  console.error(`Reading ${filename}`)
+
+  let filepath = path.resolve(language_directory, filename)
+  let language = path.basename(filename, '.yml')
+  let translation = yaml.load(fs.readFileSync(filepath, encoding))
+
+  console.log(`  "${langMatch[language]}" {
+    "Tokens" {`)
+
+  Object.keys(translation[language]).forEach((key) => {
+    let value = translation[language][key]
+
+    if (/.*\n$/.test(value)) {
+      value = value.slice(0, value.length - 1)
+    }
+
+    console.log(`      "${key}"  "${value}"`)
+  })
+
+  console.log(`    }
+  }`)
+})
+
+console.log(`}`)
